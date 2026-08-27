98b08960e582b6d16553774ec48a893a1b0b9066
axelvallin-balder <axel.vallin@skelleftea.se>
2026-08-27 14:24:09 +0200

    Added tests and example solutions

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,4 +1,10 @@
-import { uppgift1 } from './app';
+import { 
+  uppgift1, uppgift10, uppgift11, uppgift12, uppgift13, uppgift14, uppgift2, uppgift3, uppgift4, uppgift5,
+  uppgift6,
+  uppgift7,
+  uppgift8,
+  uppgift9
+ } from './app';
 
 const outputMock = jest.fn();
 const inputMock = jest.fn();
@@ -6,37 +12,365 @@ const inputMock = jest.fn();
 (globalThis as typeof globalThis & { output: jest.Mock }).output = outputMock;
 (globalThis as typeof globalThis & { input: jest.Mock }).input = inputMock;
 
-test('17 är omyndig', () => {
-  let promise = new Promise((resolve) => {
-    resolve('17')
-  })
-  inputMock.mockReturnValue(promise);
-  uppgift1().then(() => {
-    expect(outputMock).toHaveBeenCalledTimes(1);
-    expect(outputMock).toHaveBeenCalledWith('Omyndig');
-  });
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+test('[Uppgift 1] 17 är omyndig', async () => {
+  inputMock.mockResolvedValue('17');
+
+  await uppgift1();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Omyndig');
 });
 
-test('18 är myndig', () => {
-  let promise = new Promise((resolve) => {
-    resolve('18')
-  })
-  inputMock.mockReturnValue(promise);
-  uppgift1().then(() => {
-    expect(outputMock).toHaveBeenCalledTimes(1);
-    expect(outputMock).toHaveBeenCalledWith('Myndig');
-  });
-  jest.clearAllMocks()
+test('[Uppgift 1] 18 är myndig', async () => {
+  inputMock.mockResolvedValue('18');
+
+  await uppgift1();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Myndig');
 });
 
-test('99 är myndig', () => {
-  let promise = new Promise((resolve) => {
-    resolve('18')
-  })
-  inputMock.mockReturnValue(promise);
-  uppgift1().then(() => {
-    expect(outputMock).toHaveBeenCalledTimes(1);
-    expect(outputMock).toHaveBeenCalledWith('Myndig');
-  });
-  jest.clearAllMocks()
+test('[Uppgift 1] 99 är myndig', async () => {
+  inputMock.mockResolvedValue('99');
+
+  await uppgift1();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Myndig');
+});
+
+test('[Uppgift 2] 202 är lång', async () => {
+  inputMock.mockResolvedValue('202');
+
+  await uppgift2();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Hej du långe!');
+});
+
+test('[Uppgift 2] 199 är kort', async () => {
+  inputMock.mockResolvedValue('199');
+
+  await uppgift2();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Hej du korte!');
+});
+
+test('[Uppgift 3] -32 är negativt', async () => {
+  inputMock.mockResolvedValue('-32');
+
+  await uppgift3();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Talet är negativt.');
+});
+
+test('[Uppgift 3] 1 är positivt', async () => {
+  inputMock.mockResolvedValue('1');
+
+  await uppgift3();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Talet är positivt.');
+});
+
+
+test('[Uppgift 3] Talet är 0', async () => {
+  inputMock.mockResolvedValue(0);
+
+  await uppgift3();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Talet är noll.');
+});
+
+test('[Uppgift 4] x < y', async () => {
+  inputMock
+    .mockResolvedValueOnce('5')
+    .mockResolvedValueOnce('7')
+
+  await uppgift4();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('x < y');
+});
+
+test('[Uppgift 4] x > y', async () => {
+  inputMock
+    .mockResolvedValueOnce('1')
+    .mockResolvedValueOnce('-1')
+  
+  await uppgift4();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('x > y');
+});
+
+test('[Uppgift 4] x = y', async () => {
+  inputMock
+    .mockResolvedValueOnce('0')
+    .mockResolvedValueOnce('0')
+
+  await uppgift4();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('x = y');
+});
+
+
+test('[Uppgift 5] 34 + 75 = 109', async () => {
+  inputMock
+    .mockResolvedValueOnce('34')
+    .mockResolvedValueOnce('75')
+    .mockResolvedValueOnce('+')
+
+  await uppgift5();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Summa: 109');
+});
+
+
+test('[Uppgift 5] 10 - 7 = 3', async () => {
+  inputMock
+    .mockResolvedValueOnce('10')
+    .mockResolvedValueOnce('7')
+    .mockResolvedValueOnce('-')
+
+  await uppgift5();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Differens: 3');
+});
+
+test('[Uppgift 5] 9 * 9 = 81', async () => {
+  inputMock
+    .mockResolvedValueOnce('9')
+    .mockResolvedValueOnce('9')
+    .mockResolvedValueOnce('*')
+  
+  await uppgift5();
+  
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Produkt: 81');
+});
+
+test('[Uppgift 5] 1 / 4 = 0.25', async () => {
+  inputMock
+    .mockResolvedValueOnce('1')
+    .mockResolvedValueOnce('4')
+    .mockResolvedValueOnce('/')
+
+  await uppgift5();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Kvot: 0.25');
+});
+
+test('[Uppgift 6] En 7:a', async () => {
+  inputMock.mockResolvedValueOnce('7')
+
+  await uppgift6();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('En 7:a');
+});
+
+test('[Uppgift 6] Ett ess', async () => {
+  inputMock.mockResolvedValueOnce('1')
+
+  await uppgift6();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Ett ess');
+});
+
+test('[Uppgift 6] En dam', async () => {
+  inputMock.mockResolvedValueOnce('12')
+
+  await uppgift6();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('En dam');
+});
+
+test('[Uppgift 7] Oktober', async () => {
+  inputMock.mockResolvedValueOnce('10')
+
+  await uppgift7();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Oktober');
+});
+
+test('[Uppgift 7] Januari', async () => {
+  inputMock.mockResolvedValueOnce('1')
+
+  await uppgift7();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Januari');
+});
+
+test('[Uppgift 8] Höst', async () => {
+  inputMock.mockResolvedValueOnce('10')
+
+  await uppgift8();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Höst');
+});
+
+test('[Uppgift 8] Vinter', async () => {
+  inputMock.mockResolvedValueOnce('1')
+
+  await uppgift8();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Vinter');
+});
+
+test('[Uppgift 9] 11 * 12 = 130?', async () => {
+  inputMock
+    .mockResolvedValueOnce('11')
+    .mockResolvedValueOnce('12')
+    .mockResolvedValueOnce('130')
+
+  await uppgift9();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Fel. Rätt svar: 132');
+});
+
+test('[Uppgift 9] 13 * 13 = 169?', async () => {
+  inputMock
+    .mockResolvedValueOnce('13')
+    .mockResolvedValueOnce('13')
+    .mockResolvedValueOnce('169')
+
+  await uppgift9();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Rätt!');
+});
+
+test('[Uppgift 10] 4 och 7', async () => {
+  inputMock
+    .mockResolvedValueOnce('4')
+    .mockResolvedValueOnce('7')
+
+  await uppgift10();
+
+  expect(outputMock).toHaveBeenCalledTimes(4);
+  expect(outputMock).toHaveBeenCalledWith('Summa: 11');
+  expect(outputMock).toHaveBeenCalledWith('Medel: 5.5');
+  expect(outputMock).toHaveBeenCalledWith('Minsta: 4');
+  expect(outputMock).toHaveBeenCalledWith('Största: 7');
+});
+
+test('[Uppgift 11] 5, 10 och 7', async () => {
+  inputMock
+    .mockResolvedValueOnce('5')
+    .mockResolvedValueOnce('10')
+    .mockResolvedValueOnce('7')
+
+  await uppgift11();
+
+  expect(outputMock).toHaveBeenCalledTimes(4);
+  expect(outputMock).toHaveBeenCalledWith('Summa: 22');
+  expect(outputMock).toHaveBeenCalledWith(expect.stringMatching(/^Medel: 7\.3+$/));
+  expect(outputMock).toHaveBeenCalledWith('Minsta: 5');
+  expect(outputMock).toHaveBeenCalledWith('Största: 10');
+});
+
+test('[Uppgift 12] B', async () => {
+  inputMock
+    .mockResolvedValueOnce('B')
+
+  await uppgift12();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Stor bokstav');
+});
+
+test('[Uppgift 12] p', async () => {
+  inputMock
+    .mockResolvedValueOnce('p')
+
+  await uppgift12();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Liten bokstav');
+});
+
+test('[Uppgift 12] !', async () => {
+  inputMock
+    .mockResolvedValueOnce('!')
+
+  await uppgift12();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Annat tecken');
+});
+
+test('[Uppgift 13] 32', async () => {
+  inputMock
+    .mockResolvedValueOnce('32')
+
+  await uppgift13();
+
+  expect(outputMock).toHaveBeenCalledTimes(2);
+  expect(outputMock).toHaveBeenCalledWith('Två siffror');
+  expect(outputMock).toHaveBeenCalledWith('Positivt');
+});
+
+test('[Uppgift 13] -100', async () => {
+  inputMock
+    .mockResolvedValueOnce('-100')
+
+  await uppgift13();
+
+  expect(outputMock).toHaveBeenCalledTimes(2);
+  expect(outputMock).toHaveBeenCalledWith('Tre siffror');
+  expect(outputMock).toHaveBeenCalledWith('Negativt');
+});
+
+test('[Uppgift 13] 0', async () => {
+  inputMock
+    .mockResolvedValueOnce('0')
+
+  await uppgift13();
+
+  expect(outputMock).toHaveBeenCalledTimes(2);
+  expect(outputMock).toHaveBeenCalledWith('En siffra');
+  expect(outputMock).toHaveBeenCalledWith('Noll');
+});
+
+test('[Uppgift 14] Sidlängder 5, 10 och 4', async () => {
+  inputMock
+    .mockResolvedValueOnce('5')
+    .mockResolvedValueOnce('10')
+    .mockResolvedValueOnce('4')
+
+  await uppgift14();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Triangel? Nej!');
+});
+
+test('[Uppgift 14] Sidlängder 5, 6 och 7', async () => {
+  inputMock
+    .mockResolvedValueOnce('5')
+    .mockResolvedValueOnce('6')
+    .mockResolvedValueOnce('7')
+
+  await uppgift14();
+
+  expect(outputMock).toHaveBeenCalledTimes(1);
+  expect(outputMock).toHaveBeenCalledWith('Triangel? Ja!');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,176 @@
 export async function uppgift1() {
-  let ålder = +await input("Ålder: ")
+  let ålder = +await input("Ålder: ");
   if (ålder < 18) {
-    output("Omyndig")
+    output("Omyndig");
   } else {
-    output("Myndig")
+    output("Myndig");
+  }
+}
+
+export async function uppgift2() {
+  let längd = +await input("Längd: ");
+  if (längd < 200) {
+    output("Hej du korte!");
+  } else {
+    output("Hej du långe!");
+  }
+}
+
+export async function uppgift3() {
+  let x = +await input("Ett tal: ");
+  if (x < 0) {
+    output("Talet är negativt.")
+  } else if (x > 0) {
+    output("Talet är positivt.")
+  } else {
+    output("Talet är noll.")
+  }
+}
+
+export async function uppgift4() {
+  let x = +await input("x = ");
+  let y = +await input("y = ");
+  
+  if (x == y) {
+    output("x = y");
+  } else if (x < y) {
+    output("x < y");
+  } else {
+    output("x > y");
+  }
+}
+
+export async function uppgift5() {
+  let x = +await input("x = ");
+  let y = +await input("y = ");
+  let operator = await input("Operator (+, -, *, /): ");
+  
+  if (operator == "+") {
+    output("Summa: " + (x+y).toString());
+  } else if (operator == "-") {
+    output("Differens: " + (x-y).toString());
+  } else if (operator == "*") {
+    output("Produkt: " + (x*y).toString());
+  } else if (operator == "/") {
+    output("Kvot: " + (x/y).toString());
+  }
+}
+
+export async function uppgift6() {
+  let valör = +await input("Valör (1-13): ");
+
+  if (valör == 11) {
+    output("En knekt");
+  } else if (valör == 12) {
+    output ("En dam");
+  } else if (valör == 13) {
+    output("En kung");
+  } else if (valör == 1) {
+    output("Ett ess");
+  } else {
+    output("En " + valör.toString() + ":a");
+  }
+}
+
+export async function uppgift7() {
+  let nummer = +await input("Månadsnummer: ");
+  let månad = ["Januari", "Februari", "Mars", "April", "Maj", "Juni",
+    "Juli", "Augusti", "September", "Oktober", "November", "December"
+  ];
+  output(månad[nummer - 1]);
+}
+
+export async function uppgift8() {
+  let nummer = +await input("Månadsnummer: ")
+  if (nummer == 12 || nummer < 3) {
+    output("Vinter");
+  } else if (nummer > 2 && nummer < 6) {
+    output("Vår");
+  } else if (nummer > 5 && nummer < 9) {
+    output("Sommar");
+  } else {
+    output("Höst");
+  }
+}
+
+export async function uppgift9() {
+  let tal1 = +await input("Tal 1: ");
+  let tal2 = +await input("Tal 2: ");
+  let produkt = +await input("Produkt: ");
+
+  if (tal1 * tal2 == produkt) {
+    output("Rätt!");
+  } else {
+    output("Fel. Rätt svar: " + (tal1*tal2).toString());
+  }
+}
+
+export async function uppgift10() {
+  let tal1 = +await input("Tal 1: ");
+  let tal2 = +await input("Tal 2: ");
+
+  output("Summa: " + (tal1 + tal2).toString());
+  output("Medel: " + ((tal1 + tal2)/2).toString())
+  output("Minsta: " + Math.min(tal1, tal2))
+  output("Största: " + Math.max(tal1, tal2))
+}
+
+export async function uppgift11() {
+  let tal1 = +await input("Tal 1: ");
+  let tal2 = +await input("Tal 2: ");
+  let tal3 = +await input("Tal 3: ");
+
+  output("Summa: " + (tal1 + tal2 + tal3).toString());
+  output("Medel: " + ((tal1 + tal2 + tal3)/3).toString())
+  output("Minsta: " + Math.min(tal1, tal2, tal3))
+  output("Största: " + Math.max(tal1, tal2, tal3))
+}
+
+export async function uppgift12() {
+  let tecken = await input("Tecken: ")
+  if (tecken.charCodeAt(0) > 64 && tecken.charCodeAt(0) < 91) {
+    output("Stor bokstav")
+  } else if (tecken.charCodeAt(0) > 96 && tecken.charCodeAt(0) < 123) {
+    output("Liten bokstav")
+  } else {
+    output("Annat tecken")
+  }
+}
+export async function uppgift13() {
+  let tal = +await input("Mata in ett tal mellan -999 och 999: ")
+
+  if (Math.abs(tal) < 10) {
+    output("En siffra")
+  } else if (Math.abs(tal) < 100) {
+    output("Två siffror")
+  } else {
+    output("Tre siffror")
+  }
+  
+  if (tal < 0) {
+    output("Negativt")
+  } else if (tal > 0) {
+    output("Positivt")
+  } else {
+    output("Noll")
+  }
+}
+export async function uppgift14() {
+  let sida1 = +await input("Sida 1: ");
+  let sida2 = +await input("Sida 2: ");
+  let sida3 = +await input("Sida 3: ");
+
+  let negativ_sida = sida1 < 0 || sida2 < 0 || sida3 < 0
+  let sida1_plus_sida2 = sida1 + sida2
+  let sida1_plus_sida3 = sida1 + sida3
+  let sida2_plus_sida3 = sida2 + sida3
+  if (negativ_sida || 
+    sida3 > sida1_plus_sida2 || 
+    sida2 > sida1_plus_sida3 ||
+    sida1 > sida2_plus_sida3) {
+    output("Triangel? Nej!")
+  } else {
+    output("Triangel? Ja!")
   }
 }
 
